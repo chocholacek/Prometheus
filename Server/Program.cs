a57f56d28d5329dc0e7c5cec7f27d69fbada6259
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-07-07 08:24:00 -0400

    init

---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prometheus;
+
+var randomizedCounter = Metrics.CreateCounter("random_count", "How many times number (0 - 9) occured from calling Random.Next", "number");
+Random random = new ();
+
+var host = Host.CreateDefaultBuilder(args);
+
+host.ConfigureWebHostDefaults(host =>
+{
+    host.Configure(builder =>
+    {
+        builder.UseRouting();
+        builder.Use(async (ctx, next) =>
+        {
+            randomizedCounter.WithLabels($"{random.Next(10)}").Inc();
+            await next();
+        });
+        builder.UseEndpoints(endpoints => endpoints.MapMetrics());
+    });
+});
+
+await host.Build().RunAsync();
